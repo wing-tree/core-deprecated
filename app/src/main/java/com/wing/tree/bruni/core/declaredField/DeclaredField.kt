c4ef6a3b4239498916f400201c0f475825b932ac
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-12-21 22:33:32 +0900

    Added new classes

---
+++ app/src/main/java/com/wing/tree/bruni/core/declaredField/DeclaredField.kt
@@ -0,0 +1,19 @@
+package com.wing.tree.bruni.core.declaredField
+
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.wing.tree.bruni.core.extension.getDeclaredFieldOrNull
+import kotlin.reflect.KProperty
+
+class DeclaredField<T: Any?> (name: String) {
+    private val field = BottomSheetBehavior::class.getDeclaredFieldOrNull(name)
+
+    operator fun <V: View> getValue(bottomSheetBehavior: BottomSheetBehavior<V>, property: KProperty<*>): T? {
+        @Suppress("UNCHECKED_CAST")
+        return field?.get(bottomSheetBehavior) as? T
+    }
+
+    operator fun <V: View> setValue(bottomSheetBehavior: BottomSheetBehavior<V>, property: KProperty<*>, value: T) {
+        field?.set(bottomSheetBehavior, value)
+    }
+}
