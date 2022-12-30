8c699754ef31d38ab128820e40f7859a8d2eb112
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-12-30 19:13:09 +0900

    feat : Add string Flow filter extension functions

--- app/src/main/java/com/wing/tree/bruni/core/extension/Flow.kt
+++ app/src/main/java/com/wing/tree/bruni/core/extension/Flow.kt
@@ -1,6 +1,13 @@
 package com.wing.tree.bruni.core.extension
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.firstOrNull
+
+fun Flow<String>.filterNotBlank() = filter { it.isNotBlank() }
+fun Flow<String>.filterNotEmpty() = filter { it.isNotEmpty() }
+fun Flow<String?>.filterNotNullAndBlank() = filterNot { it.isNullOrBlank() }
+fun Flow<String?>.filterNotNullAndEmpty() = filterNot { it.isNullOrEmpty() }
 
 suspend fun <T> Flow<T?>.firstOrDefault(defaultValue: T): T = firstOrNull() ?: defaultValue
