47868c0bbcece4a8eff2649e4c627ce0eba4d1b6
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-01 10:19:29 +0900

    feat : Add showToast extension functions to Activity

--- app/src/main/java/com/wing/tree/bruni/core/extension/Activity.kt
+++ app/src/main/java/com/wing/tree/bruni/core/extension/Activity.kt
@@ -2,6 +2,8 @@ package com.wing.tree.bruni.core.extension
 
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.annotation.StringRes
 
 fun Activity.checkPermission(vararg permission: String): Boolean {
     return permission.all { checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }
@@ -9,4 +11,12 @@ fun Activity.checkPermission(vararg permission: String): Boolean {
 
 fun Activity.shouldShowRequestPermissionRationale(vararg permission: String): Boolean {
     return permission.any { shouldShowRequestPermissionRationale(it) }
+}
+
+fun Activity.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, resId, duration).show()
+}
+
+fun Activity.showToast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, duration).show()
 }
