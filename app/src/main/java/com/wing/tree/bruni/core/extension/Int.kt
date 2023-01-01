599e39180917175d5df6f2f9bc6f385dfa33cc5b
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-01 22:45:03 +0900

    refactor : Remove useless code

--- app/src/main/java/com/wing/tree/bruni/core/extension/Int.kt
+++ app/src/main/java/com/wing/tree/bruni/core/extension/Int.kt
@@ -8,8 +8,8 @@ import com.wing.tree.bruni.core.constant.*
 val Int.half: Int get() = div(TWO)
 val Int.isEven: Boolean get() = rem(TWO) == ZERO
 val Int.isOdd: Boolean get() = isEven.not()
-val Int.isNegative: Boolean get() = this < ZERO.float
-val Int.isPositive: Boolean get() = this > ZERO.float
+val Int.isNegative: Boolean get() = this < ZERO
+val Int.isPositive: Boolean get() = this > ZERO
 val Int.negative get() = -this
 val Int.quarter get() = div(FOUR)
 
