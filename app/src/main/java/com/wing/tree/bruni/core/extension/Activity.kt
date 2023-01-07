f6e0965154e04c3f83ab8156058488ec95cb6ba5
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-07 19:37:41 +0900

    feat : Add sharePlainText Activity extension function

--- app/src/main/java/com/wing/tree/bruni/core/extension/Activity.kt
+++ app/src/main/java/com/wing/tree/bruni/core/extension/Activity.kt
@@ -1,12 +1,23 @@
 package com.wing.tree.bruni.core.extension
 
 import android.app.Activity
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.annotation.StringRes
 
 fun Activity.checkPermission(vararg permission: String): Boolean {
     return permission.all { checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }
+}
+
+fun Activity.sharePlainText(text: CharSequence) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        putExtra(Intent.EXTRA_TEXT, text)
+        type = MIMETYPE_TEXT_PLAIN
+    }
+
+    startActivity(Intent.createChooser(intent, null))
 }
 
 fun Activity.shouldShowRequestPermissionRationale(vararg permission: String): Boolean {
