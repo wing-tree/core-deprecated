93d9d41ea419a2676b74a261785014a4decfb0dc
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-15 16:52:57 +0900

    feat : Define new Resources extension properties

--- app/src/main/java/com/wing/tree/bruni/core/extension/Resources.kt
+++ app/src/main/java/com/wing/tree/bruni/core/extension/Resources.kt
@@ -1,7 +1,13 @@
 package com.wing.tree.bruni.core.extension
 
 import android.content.res.Resources
+import androidx.annotation.*
+import androidx.core.content.res.ResourcesCompat
 
 val Resources.configShortAnimTime: Int get() = getInteger(android.R.integer.config_shortAnimTime)
 val Resources.configMediumAnimTime: Int get() = getInteger(android.R.integer.config_mediumAnimTime)
 val Resources.configLongAnimTime: Int get() = getInteger(android.R.integer.config_longAnimTime)
+
+fun Resources.dimen(@DimenRes id: Int) = getDimension(id)
+fun Resources.drawable(@DrawableRes id: Int) = ResourcesCompat.getDrawable(this, id, null)
+fun Resources.integer(@IntegerRes id: Int) = getInteger(id)
