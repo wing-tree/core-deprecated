9afb7660cd907bc388530e7aa3924444c7cc39f6
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-09 22:15:02 +0900

    refactor : Make some extension functions to public

--- windowInsetsAnimation/src/main/java/com/wing/tree/bruni/windowInsetsAnimation/extension/WindowInsetsAnimationCompat.kt
+++ windowInsetsAnimation/src/main/java/com/wing/tree/bruni/windowInsetsAnimation/extension/WindowInsetsAnimationCompat.kt
@@ -3,6 +3,6 @@ package com.wing.tree.bruni.windowInsetsAnimation.extension
 import androidx.core.view.WindowInsetsAnimationCompat
 import androidx.core.view.WindowInsetsCompat.Type.InsetsType
 
-internal fun WindowInsetsAnimationCompat.isTypeMasked(@InsetsType type: Int): Boolean {
+fun WindowInsetsAnimationCompat.isTypeMasked(@InsetsType type: Int): Boolean {
     return typeMask.isMasked(type)
 }
