a9c14b3fb9ee484a25698a997429086e7a8d1add
noah <noah@supergene.co>
2023-02-17 18:27:18 +0900

    feat : Introduce Double extension properties and functions

---
+++ app/src/main/java/com/wing/tree/bruni/core/extension/Double.kt
@@ -0,0 +1,46 @@
+package com.wing.tree.bruni.core.extension
+
+import com.wing.tree.bruni.core.constant.*
+
+val Double.complement: Double get() = run {
+    require(this in ZERO.double..ONE.double)
+
+    ONE.minus(this)
+}
+
+val Double.half: Double get() = div(TWO.double)
+val Double.hundreds: Double get() = times(ONE_HUNDRED.double)
+val Double.isNegative: Boolean get() = this < ZERO.double
+val Double.isNotNegative: Boolean get() = this >= ZERO.double
+val Double.isNotPositive: Boolean get() = this <= ZERO.double
+val Double.isPositive: Boolean get() = this > ZERO.double
+val Double.negative: Double get() = -this
+val Double.quarter get() = div(FOUR.double)
+val Double.thousands: Double get() = times(ONE_THOUSAND.double)
+
+val Double?.isZero: Boolean get() = this == ZERO.double
+val Double?.notZero: Boolean get() = isZero.not()
+
+fun Double.safeDiv(other: Double): Double {
+    return if (other.isZero) {
+        this
+    } else {
+        div(other)
+    }
+}
+
+fun Double.safeDiv(other: Int): Double {
+    return if (other.isZero) {
+        this
+    } else {
+        div(other)
+    }
+}
+
+fun Double.safeDiv(other: Long): Double {
+    return if (other.isZero) {
+        this
+    } else {
+        div(other)
+    }
+}
